9b1d51117a42b299564f60e7ef8064005f2f6ff3
cmnguyen01 <46499645+cmnguyen01@users.noreply.github.com>
2020-04-08 22:48:43 -0700

    testing

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+}
+public class enemyFactory
+{
+    public enemy getEnemy(string type)
+    {
+       if(type=="basicEnemy")
+        {
+            return new basicEnemy();
+        }
+        return null;
+    }
+}
+public interface enemy
+{
+    public void spawn();
+    public void move();
+    public void attack();
+    
+}
+public class basicEnemy : enemy
+{
+    int health;
+    int atk;
+    int speed;
+    public basicEnemy()
+    {
+        health = 10;
+        atk = 10;
+        speed = 10;
+    }
+    public void attack()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void move()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void spawn()
+    {
+        throw new System.NotImplementedException();
+    }
+}
